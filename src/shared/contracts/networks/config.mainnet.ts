0fb94d77481d2bd89a4fd22a8a2c966f45454287
Imam Ko <imamkholidubaidillah16@gmailcom>
2026-06-14 12:44:57 +0700

    fix: update missing Mainnet contract addresses causing WP Plugin to crash

--- src/shared/contracts/networks/config.mainnet.ts
+++ src/shared/contracts/networks/config.mainnet.ts
@@ -1,8 +1,8 @@
 export const mainnetConfig = {
-    contractAddress: '', // TODO: Fill after Polygon Mainnet deployment
-    Erc20Adress: '',       
-    Erc1155Adress: '',     
-    UnlockableAddress: '', 
+    contractAddress: '0xb14f5A7aB55e8C7687607bC39776fa97e983D934', // Writer
+    Erc20Adress: '0x8D8d650Cd85eD72993Fc7ea03476FDa04a1bBD2C', // LITE Token
+    Erc1155Adress: '0x753b9F10ACF325310323C86b8BdD1C5A1C00691c', // Litera Asset     
+    UnlockableAddress: '0xE8000e3BE9b0B9480De8AB82638413bE3DA97ce4', 
     usdtAddress: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F', // Real USDT Polygon
     chainId: 137,
     networkName: 'MAINNET'
