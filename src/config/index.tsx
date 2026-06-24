083b2edb702c60cc8cd4b63f20535f37c39693d0
Imam Ko <imamkholidubaidillah16@gmailcom>
2026-06-24 20:03:01 +0700

    refactor: extract RPC URL to environment variables

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -23,7 +23,7 @@ export const config = defaultWagmiConfig({
   metadata,
   transports: {
     [polygon.id]: fallback([
-        http('https://polygon-mainnet.g.alchemy.com/v2/a2qiiAva6fEQOmxhmBGSp'),
+        http(process.env.REACT_APP_ALCHEMY_RPC_URL || 'https://polygon-mainnet.g.alchemy.com/v2/a2qiiAva6fEQOmxhmBGSp'),
         http('https://polygon.llamarpc.com'),
         http('https://polygon-bor-rpc.publicnode.com'),
         http('https://1rpc.io/matic'),
